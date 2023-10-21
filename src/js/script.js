0001ec041332e83bbf7d3fb68fdac54ea9de9c70
Ramon Melo <139141993+Ramonmelod@users.noreply.github.com>
2023-10-21 13:53:38 -0300

    todas as setas direcionais completas

--- src/js/script.js
+++ src/js/script.js
@@ -2,12 +2,20 @@ const cobra = document.querySelector('.cobra');
 
 let posicaoV =18;
 let posicaoH =18;
-let idIntervalH;
-let idIntervalV;
+let idIntervalHDireita;
+let idIntervalVCima;
+let idIntervalHEsquerda;
+let idIntervalVBaixo;
 
 function contarSegundosVCima(){
     
     posicaoV ++;
+    cobra.style.bottom = posicaoV*3 +'px';
+    
+}
+
+function contarSegundosVBaixo(){
+    posicaoV --;
     cobra.style.bottom = posicaoV*3 +'px';
     
 }
@@ -19,30 +27,53 @@ function contarSegundosHDireita(){
     cobra.style.left = posicaoH*3 +'px';
     
 }
-
-
+function contarSegundosHEsquerda(){
+    
+    posicaoH --;
+    cobra.style.left = posicaoH*3 +'px';
+    
+}
 
 setInterval(document.addEventListener('keydown', (event) => {
     if (event.key === 'ArrowUp') {
 
-        clearInterval(idIntervalH);
+        clearInterval(idIntervalVBaixo);
+        clearInterval(idIntervalHDireita);
+        clearInterval(idIntervalHEsquerda);
         cobra.style.transform = 'rotate(0deg)';                //rotaciona a cobrinha para zero graus
         
-        idIntervalV = setInterval(contarSegundosVCima, 100);
+        idIntervalVCima = setInterval(contarSegundosVCima, 100);
             
-            
-        
-       
     } else if (event.key === 'ArrowRight') {
 
-        clearInterval(idIntervalV);
+        clearInterval(idIntervalVCima);
+        clearInterval(idIntervalVBaixo);
+        clearInterval(idIntervalHEsquerda);
         cobra.style.transform = 'rotate(90deg)';               //rotaciona a cobrinha para noventa graus
-        idIntervalH = setInterval(contarSegundosHDireita,100);}
+        idIntervalHDireita = setInterval(contarSegundosHDireita,100);
         
         
-    }
+    } else if (event.key === 'ArrowDown') {
+
+        clearInterval(idIntervalVCima);
+        clearInterval(idIntervalHDireita);
+        clearInterval(idIntervalHEsquerda);
+        cobra.style.transform = 'rotate(0deg)';               //rotaciona a cobrinha para noventa graus
+        idIntervalVBaixo = setInterval(contarSegundosVBaixo,100);
+        
+        
+    } else if (event.key === 'ArrowLeft') {
+
+        clearInterval(idIntervalVCima);
+        clearInterval(idIntervalHDireita);
+        clearInterval(idIntervalVBaixo);
+        cobra.style.transform = 'rotate(90deg)';               //rotaciona a cobrinha para noventa graus
+        idIntervalHEsquerda = setInterval(contarSegundosHEsquerda,100);
+        
+        
+    }}
     
-),1);
+),100);
 
 
 
